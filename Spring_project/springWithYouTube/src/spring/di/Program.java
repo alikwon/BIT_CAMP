a504f418ebd5b7a78ab5d795d7c675b2e366b8e3
alikwon <63032589+alikwon@users.noreply.github.com>
2020-07-28 20:56:52 +0900

    spring 0728 YouTube

---
+++ Spring_project/springWithYouTube/src/spring/di/Program.java
@@ -0,0 +1,31 @@
+package spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.ui.ExamConsole;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		/* 주입을 스프링에게 지시하는 방법으로 코드를변경
+		Exam exam = new NewlecExam();
+		
+		//Exam 객체(부품)를 이리저리 꽂아줌(주입)
+		// --> 이것을 대신해주는것이 스프링
+		ExamConsole console = new InlineExamConsole(exam);//DI
+		ExamConsole console = new GridExamConsole(exam);//DI
+		
+		console.setExam(exam);
+		*/
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		//ExamConsole console = (ExamConsole) context.getBean("console");
+		ExamConsole console = context.getBean("console",ExamConsole.class);
+		console.print();
+		
+		
+	}
+}
---
+++ Spring_project/springWithYouTube/src/spring/di/ui/GridExamConsole.java
@@ -0,0 +1,31 @@
+package spring.di.ui;
+
+import spring.di.entity.Exam;
+
+public class GridExamConsole implements ExamConsole {
+
+private Exam exam;
+	
+	public GridExamConsole(Exam exam) {
+		this.exam=exam;
+	}
+	
+	public GridExamConsole() {}
+	
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("┌───────────┬───────────┐");
+		System.out.println("│   total   │    avg    │");
+		System.out.println("├───────────┼───────────┤");
+		 System.out.printf("│    %3d    │   %3.2f    │\n",exam.total(),exam.avg());
+		System.out.println("└───────────┴───────────┘");
+	}
+	
+	@Override
+	public void setExam(Exam exam) {
+		// TODO Auto-generated method stub
+		this.exam=exam;
+	}
+
+}
