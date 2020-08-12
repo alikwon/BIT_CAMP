2fbce93e1af1d0e01d1e4f718062062145b9b9e9
alikwon <63032589+alikwon@users.noreply.github.com>
2020-08-12 15:10:59 +0900

    .

--- Spring_project/Mangch-chat/src/main/java/com/aia/mangch/handler/EchoHandler.java
+++ Spring_project/Mangch-chat/src/main/java/com/aia/mangch/handler/EchoHandler.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,13 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.aia.mangch.model.Member;
-
 @RequestMapping("/echo")
 public class EchoHandler extends TextWebSocketHandler {
 
 	// 세션 리스트
 	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
-
 	private static Logger logger = LoggerFactory.getLogger(EchoHandler.class);
-
+	private Map<String,String> userSessions= new HashMap<>();
 	// 클라이언트가 연결 되었을 때 실행
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -33,11 +32,15 @@ public class EchoHandler extends TextWebSocketHandler {
 	// 클라이언트가 웹소켓 서버로 메시지를 전송했을 때 실행
 	@Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        logger.info("{}로 부터 {} 받음", session.getId(), message.getPayload());
+        String[] msg = message.getPayload().split("&");
+        userSessions.put(session.getId(), msg[2]);
+//        logger.info("{}번 개시글을 보고 온 {}로 부터 {} 받음", msg[0], msg[1]);
+        System.out.println(msg[1]+"번게시글을 보고온 "+msg[2]+"가 "+msg[0]+"에게");
         //모든 유저에게 메세지 출력
-        for(WebSocketSession sess : sessionList){
-            sess.sendMessage(new TextMessage(message.getPayload()));
-        }
+//        for(WebSocketSession sess : sessionList){
+//            sess.sendMessage(new TextMessage(message.getPayload()));
+//        }
+        
 	}
 	// 클라이언트 연결을 끊었을 때 실행
 	@Override
