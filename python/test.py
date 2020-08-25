2ff29e29c6b2720c2bf7c35d76fe5621f4768b5b
alikwon <63032589+alikwon@users.noreply.github.com>
2020-08-25 16:53:40 +0900

    파이썬 첫수업

---
+++ python/test.py
@@ -0,0 +1,21 @@
+student_number = 12
+#학생 번호 출력
+print('학생번호 :',student_number)
+print(type(student_number))
+
+##########################################
+st_num_str = str(student_number)
+print('학생번호 String 형변환 :')
+print(type(st_num_str))
+
+##########################################
+number_list1 = range(10)        #0~9까지의 숫자 리스트
+number_list2 = range(5,10)      #5~9까지의 숫자 리스트
+number_list3 = range(0,10,2)    #짝수 리스트
+
+print('list1:',number_list1)
+print('list2:',number_list2)
+print('list3:',number_list3)
+'''del student_number #변수 삭제
+print('학생번호 :',student_number)'''
+
