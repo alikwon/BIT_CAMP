7e83d527c027e272a7eca777f14d02a45d73ecb6
alikwon <63032589+alikwon@users.noreply.github.com>
2020-09-10 17:52:14 +0900

    .

---
+++ bit-java-study/Test/src/test/Test2.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Test2 {
+	
+	String a;
+	
+	public static void main(String[] args) {
+		Test2 t = new Test2();
+		t.a();
+		t.b();
+	}
+	
+	public void a() {
+		a="ㅋㅋㅋ";
+	}
+	
+	public void b() {
+		System.out.print("a+1: ");
+		System.out.println(a+1);
+	}
+}
